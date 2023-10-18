118895e38c96c8b63955ae023b84f4aad9fbbabd
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2023-10-18 11:24:46 -0300

    Merge remote changes into local main branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, } from 'react-router-dom';
-import DocentesPage from './pages/docentes'; import DiscentesPage from './pages/discentes';
+import DocentesPage from './pages/docentes'; import DiscentesPage from './pages/discentes'; import InitialPage from './pages/initial';
 import ResetCSS from './style/ResetCSS';
 
 export default function App() {
@@ -8,7 +8,7 @@ export default function App() {
       <ResetCSS/>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<><h1>OK</h1></>}/>
+          <Route path='/' element={<InitialPage/>}/>
           <Route path='/docentes' element={<DocentesPage/>}/>
           <Route path='/discentes' element={<DiscentesPage/>}/>
         </Routes>
---
+++ src/pages/initial.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function InitialPage() {
+  return (
+    <div>
+      <h1>Página Inicial</h1>
+      <Link to="/discentes">Discente</Link>
+      <br></br>
+      <Link to="/docentes">Docente</Link>
+    </div>
+  );
+};
